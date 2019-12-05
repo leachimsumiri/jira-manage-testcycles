59927ce779410764c2d4d224609f1613f539f0ac
Michael Irimus <michaelirimus@gmail.com>
2019-12-05 13:49:16 +0100

    changed pkg.sh to produce 3 files for different OS

--- pkg.sh
+++ pkg.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 pwd=$(pwd)
-node ./node_modules/pkg/lib-es5/bin.js index.js --out-path $pwd -t node12-macos-x64
+node ./node_modules/pkg/lib-es5/bin.js index.js --out-path $pwd/build -t node12-macos-x64
+mv $pwd/build/index $pwd/build/macos_zephyr-clone-testcycles
+node ./node_modules/pkg/lib-es5/bin.js index.js --out-path $pwd/build -t node12-win-x64
+mv $pwd/build/index.exe $pwd/build/zephyr-clone-testcycles.exe
+node ./node_modules/pkg/lib-es5/bin.js index.js --out-path $pwd/build -t node12-linux-x64
+mv $pwd/build/index $pwd/build/linux_zephyr-clone-testcycles
