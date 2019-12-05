29fa0a3625090ee15e222ad0d2eafa3388f2b81f
Michael Irimus <michaelirimus@gmail.com>
2019-12-05 15:22:56 +0100

    Added README to build artifacts

--- pkg.sh
+++ pkg.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 pwd=$(pwd)
+
 node ./node_modules/pkg/lib-es5/bin.js index.js --out-path $pwd/build -t node12-macos-x64
 mv $pwd/build/index $pwd/build/zephyr-clone-testcycles_macos
 node ./node_modules/pkg/lib-es5/bin.js index.js --out-path $pwd/build -t node12-win-x64
 mv $pwd/build/index.exe $pwd/build/zephyr-clone-testcycles.exe
 node ./node_modules/pkg/lib-es5/bin.js index.js --out-path $pwd/build -t node12-linux-x64
 mv $pwd/build/index $pwd/build/zephyr-clone-testcycles_linux
+
+cp $pwd/README.md $pwd/build/README.md
